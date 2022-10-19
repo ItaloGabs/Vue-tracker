559d9029f11494e7fd2ae772b7a45138de46c088
Ítalo Gabriel <gabrielgg.tavares@gmail.com>
2022-10-18 21:29:15 -0300

    v0.1.0

---
+++ vue-tracker/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="colums is-one-quarter">
+      <BarraLateral/>
+      
+    </div>
+    <div class="colums is-three-quarter">
+      <Formulario/>
+
+    </div>
+
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario from './components/formulario.vue';
+
+
+
+export default defineComponent({
+    name: "App",
+    components: { BarraLateral, Formulario }
+});
+</script>
+
+<style>
+
+</style>
---
+++ vue-tracker/src/components/BarraLateral.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- /*Toda parte HTML*/ -->
+    <header>
+        <img src="../assets/logo.png" alt="">
+    </header>
+</template>
+
+<script lang="ts">
+/*Todo Typescript*/
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'BarraLateral'
+});
+</script>
+
+<style>
+/*Toda parte css*/
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
